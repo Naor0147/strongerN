a5a6cacf212e4ffe54e579c71e34a5caaae20dcd
Naor0147 <naoragam0147@gmail.com>
2026-07-13 17:09:19 +0300

    test: bypass font loading block in E2EAppHarness

--- src/screens/E2EAppHarness.tsx
+++ src/screens/E2EAppHarness.tsx
@@ -195,13 +195,7 @@ export default function E2EAppHarness() {
     return newEx;
   };
 
-  if (!fontsLoaded) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color={colors.accent} />
-      </View>
-    );
-  }
+
 
   return (
     <SafeAreaProvider>
