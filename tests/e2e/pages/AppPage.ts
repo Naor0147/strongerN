be335146ed3f693711ada4ec3e2373bb33614d32
Naor0147 <naoragam0147@gmail.com>
2026-07-13 17:02:27 +0300

    test: add browser console/error logging in E2E AppPage

--- tests/e2e/pages/AppPage.ts
+++ tests/e2e/pages/AppPage.ts
@@ -4,6 +4,12 @@ export class AppPage {
   constructor(private page: Page) {}
 
   async goto() {
+    this.page.on('console', msg => {
+      console.log(`[BROWSER CONSOLE] ${msg.type()}: ${msg.text()}`);
+    });
+    this.page.on('pageerror', err => {
+      console.log(`[BROWSER EXCEPTION]: ${err.message}\n${err.stack}`);
+    });
     await this.page.goto('/');
   }
 
